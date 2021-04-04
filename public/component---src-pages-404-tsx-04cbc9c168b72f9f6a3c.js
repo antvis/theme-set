e88d9e82ffb5d3905733bce19142b345d496e15d
visiky <736929286@qq.com>
2021-04-04 10:36:20 +0800

    chore: build site

---
+++ public/component---src-pages-404-tsx-04cbc9c168b72f9f6a3c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),o=n("N5vR");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-04cbc9c168b72f9f6a3c.js.map
