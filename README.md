5d4dd7f6421b508fa3b494eea1eb46f972dc4e8c
visiky <736929286@qq.com>
2021-04-04 10:44:32 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# About
-
-Online theme builder both for G2 and G2Plot.
--- src/theme/default.ts
+++
@@ -1,4 +0,0 @@
-import { G2 } from '@antv/g2plot';
-
-export const LIGHT_THEME = G2.getTheme('light');
-export const DARK_THEME = G2.getTheme('dark');
