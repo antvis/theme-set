39f1c386ac4d198a96f44c85d3ca41fb47876908
沈建斌 <wb-sjb709429@antfin.com>
2021-04-07 17:35:43 +0800

    feat: 完善复制到剪切板方法

--- src/utils/copy-to-board.ts
+++ src/utils/copy-to-board.ts
@@ -1,10 +1,16 @@
 // copy text to clipboard
+
+import { message } from "antd";
+
 // ref: https://stackoverflow.com/a/46118025
 export function copyToClipboard(text: string) {
   var dummy = document.createElement('input');
   document.body.appendChild(dummy);
   dummy.setAttribute('value', text);
   dummy.select();
-  document.execCommand('copy');
+  const result = document.execCommand('copy');
   document.body.removeChild(dummy);
+  if (result) {
+    message.success('复制成功');
+  }
 }
