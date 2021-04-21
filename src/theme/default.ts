6c63f012e682463b1034a29fc2daa0d354d08d28
沈建斌 <wb-sjb709429@antfin.com>
2021-04-21 21:37:47 +0800

    feat: 完善白天主题的background配置

--- src/theme/default.ts
+++ src/theme/default.ts
@@ -1,4 +1,5 @@
 import { G2 } from '@antv/g2plot';
 
-export const LIGHT_THEME = G2.getTheme('light');
+// G2 白天主题 backgroud=transparent 会造成react-color组件颜色透明度为0
+export const LIGHT_THEME = { ...G2.getTheme('light'), background: '#ffffff' };
 export const DARK_THEME = G2.getTheme('dark');
