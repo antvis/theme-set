916a52d2a4960a8d2dc2258174f3d22a621e9ba5
visiky <xinming.lxj@antgroup.com>
2022-01-24 13:52:10 +0800

    feat: 修复 footer 作者信息

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -7,14 +7,14 @@ type Props = {
   contact: string;
 };
 
-const Footer: React.FC<Props> = ({ contact, author }) => {
+const Footer: React.FC<Props> = () => {
   return (
     <footer>
       <div className="">Made with ❤️</div>
       <div className="author">
         by
-        <a href={contact} style={{ marginLeft: '4px' }}>
-          {author}
+        <a href="https://github.com/antvis" style={{ marginLeft: '4px' }}>
+          AntV
         </a>
       </div>
     </footer>
